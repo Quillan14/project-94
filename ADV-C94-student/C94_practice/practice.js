40b290b3e50ddf1ae8ecaedc48443afc79da3678
Quillan14 <128583075+Quillan14@users.noreply.github.com>
2023-10-13 18:23:05 +1100

    Add files via upload

---
+++ ADV-C94-student/C94_practice/practice.js
@@ -0,0 +1,13 @@
+
+Config = {
+  apiKey: "AIzaSyDyS9lwjrCE1m-VwGYAQNY2ONGNqRNYmnk",
+  authDomain: "kwitter-e994e.firebaseapp.com",
+  databaseURL: "https://kwitter-e994e-default-rtdb.firebaseio.com",
+  projectId: "kwitter-e994e",
+  storageBucket: "kwitter-e994e.appspot.com",
+  messagingSenderId: "1081114266041",
+  appId: "1:1081114266041:web:d0194a8a718d0a489f8e68"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
